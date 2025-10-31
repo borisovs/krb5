9cecedabe55d18332164c8bac12310c91c514885
Sergey Borisov <sborisov@astralinux.ru>
2025-10-31 16:05:15 +0300

    [WP-6196] start replacing Makefile generating *.h/*.c  by CMake generating

--- ci/mingw64_build.sh
+++ ci/mingw64_build.sh
@@ -3,15 +3,15 @@
 SELF_BIN=$(realpath ${0})
 SELF_DIR=$(dirname ${SELF_BIN})
 
-cd ${SELF_DIR}/..
-cd src
-echo Clear the old generated files..
-make -f Makefile.in clean-windows-mac
-echo Generate the new files..
-cp include/win-mac.h include/autoconf.h
-cp include/osconf.hin include/osconf.h
-make -f Makefile.in prep-windows
-rm Makefile
+# cd ${SELF_DIR}/..
+# cd src
+# echo Clear the old generated files..
+# make -f Makefile.in clean-windows-mac
+# echo Generate the new files..
+# cp include/win-mac.h include/autoconf.h
+# cp include/osconf.hin include/osconf.h
+# make -f Makefile.in prep-windows
+# rm Makefile
 
 cd ${SELF_DIR}/..
 if [ -d build ]; then
