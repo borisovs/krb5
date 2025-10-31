aa857dc38e24c155b0a982737d68f652a1374ce3
Sergey Borisov <sborisov@astralinux.ru>
2025-10-31 12:46:14 +0300

    [WP-6196] create autoconf.h osconf.h

--- ci/mingw64_build.sh
+++ ci/mingw64_build.sh
@@ -8,6 +8,8 @@ cd src
 echo Clear the old generated files..
 make -f Makefile.in clean-windows-mac
 echo Generate the new files..
+cp include/win-mac.h include/autoconf.h
+cp include/osconf.hin include/osconf.h
 make -f Makefile.in prep-windows
 rm Makefile
 
