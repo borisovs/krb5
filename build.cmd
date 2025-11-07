38aa8c81837f0001aceba46554fc6f50adf9af26
Sergey Borisov <sborisov@astralinux.ru>
2025-11-07 15:52:35 +0300

    [WP-6196] update build.cmd

--- build.cmd
+++ build.cmd
@@ -7,17 +7,19 @@ echo *******************************************
 echo:
 
 set OUTPUT=%cd%\output 
+
 if exist %OUTPUT% rmdir %OUTPUT% /q /s
+mkdir %OUTPUT%
 
 echo on
 set PATH=%PATH%;"%WindowsSdkVerBinPath%"\x86  
 set KRB_INSTALL_DIR=%OUTPUT%
-rem set OPENSSL_DIR=C:\OpenSSL-Win64              
-rem set OPENSSL_VERSION=3                         
+rem set OPENSSL_DIR=C:\OpenSSL-Win64
+rem set OPENSSL_VERSION=3
 cd src
-nmake clean                                  
-nmake -f Makefile.in prep-windows             
-nmake NODEBUG=1                             
-rem nmake install NODEBUG=1                     
-rem cd windows\installer\wix                      
-rem nmake [NODEBUG=1]                             
+nmake clean
+nmake -f Makefile.in prep-windows
+nmake NODEBUG=1
+nmake install NODEBUG=1
+rem cd windows\installer\wix
+rem nmake [NODEBUG=1]
