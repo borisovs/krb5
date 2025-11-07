eafc785422976c71f19ada2c4977794738ec9683
Sergey Borisov <sborisov@astralinux.ru>
2025-11-07 15:23:04 +0300

    [WP-6196] add build.cmd

---
+++ build.cmd
@@ -0,0 +1,23 @@
+@echo off
+echo:
+echo *******************************************
+echo *    Before build install Perl language   *
+echo *    https://strawberryperl.com/          *
+echo *******************************************
+echo:
+
+set OUTPUT=%cd%\output 
+if exist %OUTPUT% rmdir %OUTPUT% /q /s
+
+echo on
+set PATH=%PATH%;"%WindowsSdkVerBinPath%"\x86  
+set KRB_INSTALL_DIR=%OUTPUT%
+rem set OPENSSL_DIR=C:\OpenSSL-Win64              
+rem set OPENSSL_VERSION=3                         
+cd src
+nmake clean                                  
+nmake -f Makefile.in prep-windows             
+nmake NODEBUG=1                             
+rem nmake install NODEBUG=1                     
+rem cd windows\installer\wix                      
+rem nmake [NODEBUG=1]                             
