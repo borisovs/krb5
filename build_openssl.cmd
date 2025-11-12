e0d6cdf307646e57575cf4a091fc4234d9b2c86a
Sergey Borisov <sborisov@astralinux.ru>
2025-11-12 12:33:39 +0300

    [WP-6196] update build_openssl.cmd

--- build_openssl.cmd
+++ build_openssl.cmd
@@ -9,14 +9,15 @@ echo:
 set SRC_DIR=%cd%\openssl_src\
 if exist %SRC_DIR% rmdir %SRC_DIR% /q /s
 git clone --recursive --depth 1 --single-branch --branch openssl-3.6.0 https://github.com/openssl/openssl.git %SRC_DIR%
-cd %SRC_DIR%
 
 set PATH=%PATH%;"C:\Program Files\NASM"
 set PATH=%PATH%;"C:\Strawberry\perl\bin"
 
-set %OPENSSL_DIR%=%cd%\openssl\
+set OPENSSL_DIR=%cd%\openssl\
 if exist %OPENSSL_DIR% rmdir %OPENSSL_DIR% /q /s
 mkdir %OPENSSL_DIR%
 
-perl Configure --prefix=%OPENSSL_DIR% VC-WIN64A no-shared enable-capieng
+cd %SRC_DIR%
+perl Configure --prefix=%OPENSSL_DIR% --openssldir=%OPENSSL_DIR% VC-WIN64A no-shared enable-capieng
 nmake
+nmake install_sw
